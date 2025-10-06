e86eb5c8bd18e47bde9a582f3d4de08522b15285
RAJUYADAV03 <rajumogili4528@gmail.com>
2025-10-06 19:48:55 +0530

    Create FabricRestapiapp.java

---
+++ FabricRestapiapp.java
@@ -0,0 +1,12 @@
+package com.example.fabricrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricRestApiApplication.class, args);
+    }
+}
